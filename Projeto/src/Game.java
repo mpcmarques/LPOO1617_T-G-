67a708eaf3d7ea540ceae28d6e63fefc752f82f7
Mateus <mpcmarques@icloud.com>
2017-03-07 14:49:04 +0000

    GAME ADDED

---
+++ Projeto/src/Game.java
@@ -0,0 +1,30 @@
+
+public class Game {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//	Line 1
+		System.out.println("|X|X|X|X|X|X|X|X|X|X|");
+		//	Line 2
+		System.out.println("|X|H| | |I| |X| |G|X|");
+		//	Line 3
+		System.out.println("|X|X|X| |X|X|X| | |X|");
+		//	Line 4
+		System.out.println("|X| |I| | |I|X| | |X|");
+		//	Line 5
+		System.out.println("|X|X|X| |X|X|X| | |X|");
+		//	Line 6
+		System.out.println("|I| | | | | | | | | |");
+		//	Line 7
+		System.out.println("|I| | | | | | | | | |");
+		//	Line 8
+		System.out.println("|X|X|X| |X|X|X|X| |X|");
+		// Line 9
+		System.out.println("|X| |I| |I| |X|K| |X|");
+		// Line 10
+		System.out.println("|X|X|X|X|X|X|X|X|X|X|");
+	}
+
+}
