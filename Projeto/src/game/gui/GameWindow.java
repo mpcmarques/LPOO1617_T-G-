a8cd4ddc04849b864354e15720ce8a77b438b206
Mateus <mpcmarques@icloud.com>
2017-03-26 21:17:03 +0100

    refactored

--- Projeto/src/game/gui/GameWindow.java
+++ Projeto/src/game/gui/GameWindow.java
@@ -61,12 +61,6 @@ public class GameWindow extends JFrame {
 		getContentPane().setLayout(new MigLayout("", "[][][][][grow]", "[grow][grow][][][][][][grow][][][grow][][][grow][][grow]"));
 
 		this.btnEditGame = new JButton("Edit game");
-		btnEditGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Edit game
-				editGame();
-			}
-		});
 
 		btnNewGame = new JButton("New game");
 		btnNewGame.setFocusable(false);
@@ -84,148 +78,175 @@ public class GameWindow extends JFrame {
 		getContentPane().add(lblGeneral, "cell 0 2,alignx left");
 
 		this.btnRemoveElement = new JButton("Remove");
-		btnRemoveElement.setEnabled(false);
-		btnRemoveElement.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Remove element
-				gamePanel.setMouseAction(GameWindowEditMouseActions.removeElement);
-			}
-		});
-		btnRemoveElement.setFocusable(false);
-
 		getContentPane().add(btnRemoveElement, "cell 0 3");
 
 		this.btnAddDoor = new JButton("Add Door");
-		btnAddDoor.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Add door
-				gamePanel.setMouseAction(GameWindowEditMouseActions.addDoor);
-			}
-		});
-		btnAddDoor.setFocusable(false);
-		btnAddDoor.setEnabled(false);
 		getContentPane().add(btnAddDoor, "cell 0 4,alignx left,aligny center");
 
 		this.btnAddPilar = new JButton("Add Pilar");
-		btnAddPilar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Add pilar
-				gamePanel.setMouseAction(GameWindowEditMouseActions.addPilar);
-			}
-		});
-		btnAddPilar.setFocusable(false);
-		btnAddPilar.setEnabled(false);
 		getContentPane().add(btnAddPilar, "cell 1 4,alignx left");
 
 		this.btnAddWall = new JButton("Add Wall");
-		btnAddWall.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Add Wall
-				gamePanel.setMouseAction(GameWindowEditMouseActions.addWall);
-			}
-		});
-		btnAddWall.setEnabled(false);
-		btnAddWall.setFocusable(false);
 		getContentPane().add(btnAddWall, "cell 0 5,alignx left");
 
 		this.btnAddHero = new JButton("Add Hero");
-		btnAddHero.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Add Hero
-				gamePanel.setMouseAction(GameWindowEditMouseActions.addHero);
-			}
-		});
-		btnAddHero.setEnabled(false);
-		btnAddHero.setFocusable(false);
 		getContentPane().add(btnAddHero, "cell 1 5,alignx left");
 
 		this.btnAddNewRow = new JButton("Add New Line");
-		btnAddNewRow.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// Add new line
-				addLine();
-			}
-		});
-		btnAddNewRow.setEnabled(false);
-		btnAddNewRow.setFocusable(false);
 		getContentPane().add(btnAddNewRow, "cell 0 6,alignx left");
 
 		this.btnAddNewColumn = new JButton("Add New Column");
-		btnAddNewColumn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Adds new row
-				addRow();
-			}
-		});
-		btnAddNewColumn.setEnabled(false);
-		btnAddNewColumn.setFocusable(false);
 		getContentPane().add(btnAddNewColumn, "cell 1 6,alignx left");
 
 		JLabel lblFirstLevel = new JLabel("First Level");
 		getContentPane().add(lblFirstLevel, "cell 0 8,alignx left");
 
 		this.btnAddLever = new JButton("Add Lever");
-		btnAddLever.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Add lever
-				gamePanel.setMouseAction(GameWindowEditMouseActions.addLever);
-			}
-		});
-		btnAddLever.setEnabled(false);
-		btnAddLever.setFocusable(false);
 		getContentPane().add(btnAddLever, "cell 0 9,alignx left,aligny center");
 
 		this.btnAddGuard = new JButton("Add Guard");
-		btnAddGuard.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Add lever
-				gamePanel.setMouseAction(GameWindowEditMouseActions.addGuard);
-			}
-		});
-		btnAddGuard.setEnabled(false);
-		btnAddGuard.setFocusable(false);
 		getContentPane().add(btnAddGuard, "cell 1 9,alignx left");
 
 		JLabel lblSecondLevel = new JLabel("Second Level");
 		getContentPane().add(lblSecondLevel, "cell 0 11,alignx left");
 
 		this.btnAddOgre = new JButton("Add Ogre");
-		btnAddOgre.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Add ogre
-				gamePanel.setMouseAction(GameWindowEditMouseActions.addOgre);
-			}
-		});
-		btnAddOgre.setFocusable(false);
-		btnAddOgre.setEnabled(false);
 		getContentPane().add(btnAddOgre, "cell 0 12");
 
 		this.btnNewButton = new JButton("Add Key");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//	Add key
-				gamePanel.setMouseAction(GameWindowEditMouseActions.addKey);
-			}
-		});
-		btnNewButton.setFocusable(false);
-		btnNewButton.setEnabled(false);
 		getContentPane().add(btnNewButton, "cell 1 12");
 
 		JLabel lblApplyChanges = new JLabel("Apply Changes");
 		getContentPane().add(lblApplyChanges, "cell 0 14,alignx left");
 
 		this.btnApply = new JButton("Apply");
+		getContentPane().add(btnApply, "cell 0 15,alignx left");
+
+		setVisible(true);
+		
+		//	Add buttons actions
+		this.addButtonsActionListeners();
+		//	Remove buttons focusable
+		this.removeButtonsFocusable();
+	}
+	/** 
+	 * Set buttons no focusable
+	 * */
+	private void removeButtonsFocusable(){
+		btnRemoveElement.setEnabled(false);
+		btnRemoveElement.setFocusable(false);
+		btnAddDoor.setFocusable(false);
+		btnAddDoor.setEnabled(false);
+		btnAddPilar.setFocusable(false);
+		btnAddPilar.setEnabled(false);
+		btnAddHero.setEnabled(false);
+		btnAddHero.setFocusable(false);
+		btnAddWall.setEnabled(false);
+		btnAddWall.setFocusable(false);
+		btnAddNewRow.setEnabled(false);
+		btnAddNewRow.setFocusable(false);
+		btnAddNewColumn.setEnabled(false);
+		btnAddNewColumn.setFocusable(false);
+		btnAddLever.setEnabled(false);
+		btnAddLever.setFocusable(false);
+		btnApply.setEnabled(false);
+		btnApply.setFocusable(false);
+		btnNewButton.setFocusable(false);
+		btnNewButton.setEnabled(false);
+		btnAddOgre.setFocusable(false);
+		btnAddOgre.setEnabled(false);
+		btnAddGuard.setEnabled(false);
+		btnAddGuard.setFocusable(false);
+	}
+	
+	/** 
+	 * Adds buttons actions
+	 * */
+	private void addButtonsActionListeners(){
+		btnEditGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Edit game
+				editGame();
+			}
+		});
+		btnRemoveElement.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Remove element
+				gamePanel.setMouseAction(GameWindowEditMouseActions.removeElement);
+			}
+		});
+		btnAddDoor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add door
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addDoor);
+			}
+		});
+		btnAddDoor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add door
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addDoor);
+			}
+		});
+		btnAddPilar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add pilar
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addPilar);
+			}
+		});
+		btnAddWall.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add Wall
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addWall);
+			}
+		});
+		btnAddHero.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add Hero
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addHero);
+			}
+		});
+		btnAddNewRow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Add new line
+				addLine();
+			}
+		});
+		btnAddNewColumn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Adds new row
+				addRow();
+			}
+		});
+		btnAddLever.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add lever
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addLever);
+			}
+		});
+		btnAddGuard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add lever
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addGuard);
+			}
+		});
+		btnAddOgre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add ogre
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addOgre);
+			}
+		});
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//	Add key
+				gamePanel.setMouseAction(GameWindowEditMouseActions.addKey);
+			}
+		});
 		btnApply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Apply game changes
 				applyGameChanges();
 			}
 		});
-		btnApply.setEnabled(false);
-		btnApply.setFocusable(false);
-		getContentPane().add(btnApply, "cell 0 15,alignx left");
-
-		setVisible(true);
 	}
 
 	/** 
--- README.md
+++ README.md
@@ -11,5 +11,5 @@ Pasta da documentação: doc
 
 <a href="https://ibb.co/bYQzwF"><img src="https://preview.ibb.co/hqmTqa/Captura_de_ecr_2017_03_26_s_20_50_15.png" border="0"></a>
 
-<a href="https://ibb.co/is2ZVa"><img src="https://preview.ibb.co/cTy8qa/Captura_de_ecr_2017_03_26_s_20_55_42.png" alt="Captura_de_ecr_2017_03_26_s_20_55_42" border="0"></a><br /><a 
+<a href="https://ibb.co/is2ZVa"><img src="https://preview.ibb.co/cTy8qa/Captura_de_ecr_2017_03_26_s_20_55_42.png" alt="Captura_de_ecr_2017_03_26_s_20_55_42" border="0"></a><br /><a>
 
